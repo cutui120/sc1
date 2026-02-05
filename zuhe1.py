0708cdf0af4c0bd8d57513dbb58ae82d31d8a0ac
cutui120 <123867202+cutui120@users.noreply.github.com>
2026-02-05 18:35:18 +0800

    Update zuhe1.py

--- zuhe1.py
+++ zuhe1.py
@@ -16,9 +16,6 @@ from scipy import stats
 import shap
 import os
 
-# 设置中文字体支持
-plt.rcParams['font.sans-serif'] = ['SimHei', 'Microsoft YaHei', 'DejaVu Sans']
-plt.rcParams['axes.unicode_minus'] = False
 
 warnings.filterwarnings('ignore')
 optuna.logging.set_verbosity(optuna.logging.WARNING)
@@ -1183,3 +1180,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
